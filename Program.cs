a3cfac1cb1410823e60d5c0f94c991815a420f0c
Isamilmaher <ismailmaher6977@gmail.com>
2026-03-05 15:21:45 +0200

    Question 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,45 @@
-﻿namespace G_Net_34_CS05
+﻿using System.Security.Cryptography;
+using System.Threading.Channels;
+
+namespace G_Net_34_CS05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Question 1
+            //int dayNumber = 0;
+            //bool isValidInput = false;
+
+            //do
+            //{
+            //    Console.WriteLine("Enter a day number (1-7): \n");
+            //    dayNumber = int.Parse(Console.ReadLine());
+            //    if (dayNumber >= 1 && dayNumber <= 7)
+            //    {
+            //        DayOfWeek selectDay = (DayOfWeek)dayNumber;
+            //        Console.WriteLine($"Day:{selectDay}");
+
+            //        switch (selectDay)
+            //        {
+            //            case DayOfWeek.friday:
+            //            case DayOfWeek.satarday:
+            //                Console.WriteLine($"It is a Weekend");
+            //                break;
+            //            default:
+            //                Console.WriteLine($"It is a workday");
+            //                break;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Invalid day number. Please enter a number between 1 and 7.\n");
+            //    }
+            //} while (!isValidInput);   
+            #endregion
+
+
+
         }
     }
 }
